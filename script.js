5c5eb199d2a74dd7a58e7bfdb2da630e37c60221
Bryan Ha <hxha@ucsd.edu>
2020-05-11 22:31:43 -0700

    add some javascript for the random btn

--- script.js
+++ script.js
@@ -40,123 +40,130 @@ $(document).ready(function(){
             // tequila, vodka, gin, whiskey, rum 
         
     })
+    //break
+    // $(".stepTwo-container").on("click", ".liquor-btn", function(){
+    //     var userChoice = $(this).val();
+    //     var queryUrl = baseUrl + "filter.php?i=" + userChoice;
 
-    $(".stepTwo-container").on("click", ".liquor-btn", function(){
-<<<<<<< HEAD
-        console.log($(this).val());
-    })
-        // var searchTerm = $(this).val()
-        // if (searchTerm == Tequila) {}
-        // randomBtn.on("click", function(){
-    randomBtn.on("click", function(){
-        //also hide the dropdown button
-        $(".dropdown").attr("style", "display:none");
-        var randomDrinks = ["Clover Club", "City Slicker", "Brandy Alexander", "Affair", "Mimosa", "Shanghai Cocktail"," Scotch Cobbler", "Run Milk Punch", "Army Special", "Hot Chocolate to Die for"];
-        // // for (var i=0; i<randomDrinks.length; i++){
-    
-        var selectedRandomDrink = Math.floor(Math.random()*10)+1;
-        console.log(selectedRandomDrink);
-        var youSelected = "Here is your drink " + randomDrinks[selectedRandomDrink];
-        console.log(youSelected);
-=======
-        var userChoice = $(this).val();
-        var queryUrl = baseUrl + "filter.php?i=" + userChoice;
+    //     $.ajax({
+    //         url: queryUrl,
+    //         method: "GET"
+    //     }).then(function(cocktails){;
+    //         console.log(cocktails);
+    //         var drinkOptions = cocktails.drinks // this is an array
+    //         var randomSelection = drinkOptions[Math.floor(Math.random()*drinkOptions.length)];
+    //         console.log(randomSelection);
 
-        $.ajax({
-            url: queryUrl,
-            method: "GET"
-        }).then(function(cocktails){;
-            console.log(cocktails);
-            var drinkOptions = cocktails.drinks // this is an array
-            var randomSelection = drinkOptions[Math.floor(Math.random()*drinkOptions.length)];
-            console.log(randomSelection);
+    //         $(".row-1").attr("style", "display: block");
+    //         $(".row-2").attr("style", "display: block");
+    //         $(".stepTwo-container").attr("style", "display: none");
+    //         $(".your-drink").append(randomSelection.strDrink);
 
-            $(".row-1").attr("style", "display: block");
-            $(".row-2").attr("style", "display: block");
-            $(".stepTwo-container").attr("style", "display: none");
-            $(".your-drink").append(randomSelection.strDrink);
+    //         var img = $("<img src='" + randomSelection.strDrinkThumb + "' class='drinkImg img-fluid clearfix' />");
+    //         console.log(randomSelection.strDrinkThumb);
+    //         $(".drink-image").append(img);
 
-            var img = $("<img src='" + randomSelection.strDrinkThumb + "' class='drinkImg img-fluid clearfix' />");
-            console.log(randomSelection.strDrinkThumb);
-            $(".drink-image").append(img);
+    //         var displayCocktail = randomSelection.idDrink;
 
-            var displayCocktail = randomSelection.idDrink;
+    //         getRecipe(displayCocktail);
 
-            getRecipe(displayCocktail);
+    //     })
 
-        })
->>>>>>> 33b588cd0d9c8ee837e9c4b6f0c536433b4866c2
-
-        $.ajax({
-            type: "GET",
-            url: "https://www.thecocktaildb.com/api/json/v1/1/random.php"
-        })
+    //     $.ajax({
+    //         type: "GET",
+    //         url: "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+    //     })
         
-        // }
-    })
-<<<<<<< HEAD
-=======
+    //     // }
+    // })
 
-    function getRecipe(x) {
-        var recipeUrl = baseUrl + "lookup.php?i=" + x;
-        console.log(recipeUrl);
-        var options = {
-            "async": true,
-            "crossDomain": true,
-            "url": recipeUrl,
-            "method": "GET",
-        }
+    // function getRecipe(x) {
+    //     var recipeUrl = baseUrl + "lookup.php?i=" + x;
+    //     console.log(recipeUrl);
+    //     var options = {
+    //         "async": true,
+    //         "crossDomain": true,
+    //         "url": recipeUrl,
+    //         "method": "GET",
+    //     }
         
-        $.ajax(options).then(function(response){
-            console.log(response);
-            var drinkRecipe = response.drinks[0];
+    //     $.ajax(options).then(function(response){
+    //         console.log(response);
+    //         var drinkRecipe = response.drinks[0];
 
-            var allIngredients = [
-                {ingredient: drinkRecipe.strIngredient1, measure: drinkRecipe.strMeasure1},
-                {ingredient: drinkRecipe.strIngredient2, measure: drinkRecipe.strMeasure2},
-                {ingredient: drinkRecipe.strIngredient3, measure: drinkRecipe.strMeasure3},
-                {ingredient: drinkRecipe.strIngredient4, measure: drinkRecipe.strIngredient5}, 
-                {ingredient: drinkRecipe.strIngredient6, measure: drink
-                drinkRecipe.strIngredient7, drinkRecipe.strIngredient8, drinkRecipe.strIngredient9,
-                drinkRecipe.strIngredient10, drinkRecipe.strIngredient11, drinkRecipe.strIngredient12,
-                drinkRecipe.strIngredient13, drinkRecipe.strIngredient14, drinkRecipe.strIngredient15
-            ];
+    //         var allIngredients = [
+    //             {ingredient: drinkRecipe.strIngredient1, measure: drinkRecipe.strMeasure1},
+    //             {ingredient: drinkRecipe.strIngredient2, measure: drinkRecipe.strMeasure2},
+    //             {ingredient: drinkRecipe.strIngredient3, measure: drinkRecipe.strMeasure3},
+    //             {ingredient: drinkRecipe.strIngredient4, measure: drinkRecipe.strIngredient5}, 
+    //             {ingredient: drinkRecipe.strIngredient6, measure: drink
+    //             drinkRecipe.strIngredient7, drinkRecipe.strIngredient8, drinkRecipe.strIngredient9,
+    //             drinkRecipe.strIngredient10, drinkRecipe.strIngredient11, drinkRecipe.strIngredient12,
+    //             drinkRecipe.strIngredient13, drinkRecipe.strIngredient14, drinkRecipe.strIngredient15
+    //         ];
 
-            // var allMeasures = [
-            //     drinkRecipe.strMeasure1, drinkRecipe.strMeasure2, drinkRecipe.strMeasure3,
-            //     drinkRecipe.strMeasure4, drinkRecipe.strMeasure5, drinkRecipe.strMeasure6,
-            //     drinkRecipe.strMeasure7, drinkRecipe.strMeasure8, drinkRecipe.strMeasure9,
-            //     drinkRecipe.strMeasure10, drinkRecipe.strMeasure11, drinkRecipe.strMeasure12,
-            //     drinkRecipe.strMeasure13, drinkRecipe.strMeasure14, drinkRecipe.strMeasure15
-            // ];
+    //         // var allMeasures = [
+    //         //     drinkRecipe.strMeasure1, drinkRecipe.strMeasure2, drinkRecipe.strMeasure3,
+    //         //     drinkRecipe.strMeasure4, drinkRecipe.strMeasure5, drinkRecipe.strMeasure6,
+    //         //     drinkRecipe.strMeasure7, drinkRecipe.strMeasure8, drinkRecipe.strMeasure9,
+    //         //     drinkRecipe.strMeasure10, drinkRecipe.strMeasure11, drinkRecipe.strMeasure12,
+    //         //     drinkRecipe.strMeasure13, drinkRecipe.strMeasure14, drinkRecipe.strMeasure15
+    //         // ];
 
-            $(".glass-type").append(drinkRecipe.strGlass);
+    //         $(".glass-type").append(drinkRecipe.strGlass);
 
-            for (var i=0; i < allIngredients.length; i++) {
-                if (allIngredients[i] == null) {
-                    return;
-                } else {
-                    for (var i=0; i < allMeasures.length; i++) {
-                        // if (allMeasures[i] !== null) {
-                            $(".ingredient-list").append("<li>" + allMeasures[i] + allIngredients[i] + "</li>" )   
-                        // }
-                    }
-                }
-            }
-            // $(".ingredient-list").append("<li>" + drinkRecipe.strIngredient1 + "," + drinkRecipe.strMeasure1 + "</li>");
-            // $(".ingredient-list").append("<li>" + drinkRecipe.strIngredient2 + "," + drinkRecipe.strMeasure2 + "</li>");
+    //         for (var i=0; i < allIngredients.length; i++) {
+    //             if (allIngredients[i] == null) {
+    //                 return;
+    //             } else {
+    //                 for (var i=0; i < allMeasures.length; i++) {
+    //                     // if (allMeasures[i] !== null) {
+    //                         $(".ingredient-list").append("<li>" + allMeasures[i] + allIngredients[i] + "</li>" )   
+    //                     // }
+    //                 }
+    //             }
+    //         }
+    //         // $(".ingredient-list").append("<li>" + drinkRecipe.strIngredient1 + "," + drinkRecipe.strMeasure1 + "</li>");
+    //         // $(".ingredient-list").append("<li>" + drinkRecipe.strIngredient2 + "," + drinkRecipe.strMeasure2 + "</li>");
 
-            $(".instructions").append(drinkRecipe.strInstructions);
-        })
+    //         $(".instructions").append(drinkRecipe.strInstructions);
+    //     })
         
-    }
-
->>>>>>> 33b588cd0d9c8ee837e9c4b6f0c536433b4866c2
+    // }
+    //break
     // categoryBtn.on("click", function(){
     //     // Hide dropdown
     //     // Populate new item that looks like original button
     //         // Underneath populate box buttons each with a different category
     //         // Show input with selection box for music 
     // })
+    // categoryBtn.on("click", function(){
+    //     // Hide dropdown
+    //     // Populate new item that looks like original button
+    //         // Underneath populate box buttons each with a different category
+    //         // Show input with selection box for music 
+    // })
+    randomBtn.on("click", function(){
+        $(".dropdown").attr("style","display:none");
+        var url = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+
+        $.ajax({
+            url: url,
+            method:"GET",
+            success: function(getRandomDrink){
+                console.log(getRandomDrink.drinks[0].strDrink);
+
+                drinkName = getRandomDrink.drinks[0].strDrink;
+                $(".your-drink").append(drinkName);
+                $(".row-2").attr("style","display:block");
+
+                drinkImg = getRandomDrink.drinks[0].strDrinkThumb;
+                console.log(drinkImg);
+                $(".img").attr("src", drinkImg);
+                
+
+            }
+        })
+    })
 
 })
